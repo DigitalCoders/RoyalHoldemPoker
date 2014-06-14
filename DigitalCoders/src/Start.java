967e2829af010ddd6f4e88167638a6608aefd486
chanaka-13208296 <chanaka.ilangasinghe@yahoo.com>
2014-06-14 21:19:30 +0530

    cheking
    updated printing string

--- DigitalCoders/src/Start.java
+++ DigitalCoders/src/Start.java
@@ -2,7 +2,7 @@
 public class Start {
 
 	public static void main(String[] args) {
-		System.out.println("hello world we are Digital Coders");
+		System.out.println("hello world we are Digital Coders :))))");
 	System.out.println("test 1 for github");
 	}
 
