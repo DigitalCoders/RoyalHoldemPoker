fc03e195c8cdd5e2e3277c72e41705f6f490a148
Inesh-13208383 <Imesha@192.168.1.140>
2014-06-15 11:37:56 +0530

    start coding in dealer

--- DigitalCoders/src/Dealer.java
+++ DigitalCoders/src/Dealer.java
@@ -1,4 +1,15 @@
 
+
 public class Dealer {
+		Deck cardDeck;	// d=reference to card deck
+		double betCollection;	// total money for bet 	
+		Player pyrs[]=new Player[5];
+		public Dealer() {
+			cardDeck=new Deck();
+			betCollection=0;
+		}
+		
+		
+		
 		
 }
