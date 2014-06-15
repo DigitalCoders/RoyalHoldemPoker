96ad02d12bdd741e97c68421e37620c020f516e5
Chanaka <Chanaka@Chanaka-PC>
2014-06-15 21:39:20 +0530

    added a new out put for test

--- DigitalCoders/src/Start.java
+++ DigitalCoders/src/Start.java
@@ -4,6 +4,7 @@ public class Start {
 	public static void main(String[] args) {
 		System.out.println("hello world we are Digital Coders :))))");
 		System.out.println("test 1 for github###");
+		System.out.println("test 1 for github by chana");
 		
 		Deck d1=new Deck();
 		//d1.showDeck();
