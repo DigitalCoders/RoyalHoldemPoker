6370d0fa20fc25cda8fe559f66e16106b0607654
Inesh-13208383 <Imesha@192.168.1.140>
2014-06-16 10:36:19 +0530

    add code for set of card to initialize and give these to players and put to table

--- DigitalCoders/src/Dealer.java
+++ DigitalCoders/src/Dealer.java
@@ -1,15 +1,50 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+
 
 
 public class Dealer {
-		Deck cardDeck;	// d=reference to card deck
-		double betCollection;	// total money for bet 	
-		Player pyrs[]=new Player[5];
-		public Dealer() {
-			cardDeck=new Deck();
-			betCollection=0;
+	Deck cardDeck;	// d=reference to card deck
+	double betCollection;	// total money for bet 	
+	public static int crdpostn=0;
+	private ArrayList<Card> allCards;
+
+
+	public Dealer() {
+		allCards = new ArrayList<Card>();
+
+		for (short suit=0; suit<=3; suit++)		//go through 4 suits
+		{
+			for (short rank=0; rank<=4; rank++)		//go through 5 ranks
+			{
+				allCards.add( new Card(suit,rank) );		//add new card to allCard Array
+			}
 		}
-		
-		//testing
-		
-		
+
+		Collections.shuffle(allCards);	//Shuffling the card deck
+
+		betCollection=0;
+		crdpostn=0;
+	}
+	public void shuffleDeck(){
+		Collections.shuffle(allCards);	//Shuffling the card deck
+	}
+	public void showDeck(){
+		Iterator<Card> iterator = allCards.iterator();
+		while (iterator.hasNext()) {
+			iterator.next().showCard();
+		}
+	}
+
+	public Card getCard(int cardNo){
+
+		return allCards.get(cardNo);
+	}
+	public Card getNextCard(){
+		Card nxtcrd= allCards.get(crdpostn);
+		crdpostn++;
+		return nxtcrd;
+	}
+
 }
