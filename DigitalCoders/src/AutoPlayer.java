9850bd6a3df35621748a3e8ccd38f0f198f6f58f
Chanaka <Chanaka@Chanaka-PC>
2014-06-17 05:45:54 +0530

    Added a new structure to AI(not yet implimented)

---
+++ DigitalCoders/src/AutoPlayer.java
@@ -0,0 +1,12 @@
+public class AutoPlayer{
+	
+public AutoPlayer(Card cardone,Card cardtwo ){
+	cardone = new Card(); 
+	cardtwo = new Card();
+	}
+public AutoPlayer(Card frmtbl){
+	frmtbl = new Card();
+}
+	
+
+}
