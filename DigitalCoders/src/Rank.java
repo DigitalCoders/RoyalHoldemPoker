1d94ab457c8d786a106c3a66cc7254c1088498be
Isuru-13208384 <isuru.m.silva@gmail.com>
2014-06-17 15:58:21 +0530

    Update Rank.java
    add comments to implemeted methods

--- DigitalCoders/src/Rank.java
+++ DigitalCoders/src/Rank.java
@@ -6,11 +6,11 @@ public class Rank {
 
 	public int chkRank(List<Card> tblcrds,Card crd1,Card crd2){
 		int rank=0;
-		ArrayList<Card> cardSet=(ArrayList<Card>) tblcrds;
+		ArrayList<Card> cardSet=(ArrayList<Card>) tblcrds;	// add all the cards (table cards and players card) to list
 		cardSet.add(crd1);
 		cardSet.add(crd2);
 		
-		for (Card card : cardSet) {
+		for (Card card : cardSet) {		//to print outh the cards
 			card.showCard();
 			
 		}
