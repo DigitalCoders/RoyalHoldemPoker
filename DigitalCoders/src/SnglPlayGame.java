91062e20a69c039860fd170a2f7f3f0883eb84a5
Inesh-13208383 <Imesha@192.168.1.140>
2014-06-24 22:04:02 +0530

    reorganized the code

--- DigitalCoders/src/SnglPlayGame.java
+++ DigitalCoders/src/SnglPlayGame.java
@@ -8,6 +8,8 @@ public class SnglPlayGame {
 	Dealer delr;
 	boolean allSameBet=false;
 	int blncMnyPlyrs=0;
+	int plyrInGame=0;
+	int foldPlyrs=0;
 	
 	public void startGame(){
 		players=new ArrayList<Player>();
@@ -21,18 +23,12 @@ public class SnglPlayGame {
 		Player p2=players.get(1);
 		p2.setBgBlnd(true);
 		
+		plyrInGame=players.size();
 		
 		Dealer dlr=new Dealer();
 		dlr.shuffleDeck();
 		Table tbl=new Table();
-		for (Player plr : players) {
-			plr.set1StCard(dlr.getNextCard());			
-		}
-		
-		for (Player plr : players) {
-			plr.set2NdCard(dlr.getNextCard());
-			
-		}
+		setCards(tbl, dlr, players);
 		for (Player plr : players) {
 				if (plr.isSmlblnd()) {
 					plr.postSmlblnd(dlr);
@@ -53,6 +49,12 @@ public class SnglPlayGame {
 		}
 		System.out.println("deler done the job put 3 cards to table");
 		tbl.showTablCrd();
+		tbl.SetDeck(dlr.getNextCard());
+		System.out.println("dealer put the next car also now start betting again");
+		tbl.showTablCrd();	
+		System.out.println("currentlu players in the game "+plyrInGame);
+		System.out.println("players folden"+foldPlyrs);
+		
 		
 //		dlr.showDeck();
 //		for (Player plr : players) {
@@ -64,19 +66,29 @@ public class SnglPlayGame {
 		
 		
 	}
+	private void setCards(Table tbl,Dealer dlr,List<Player> players){
+		for (Player plr : players) {
+			plr.set1StCard(dlr.getNextCard());			
+		}
+		
+		for (Player plr : players) {
+			plr.set2NdCard(dlr.getNextCard());
+			}
+	}
 	private void betting(Table tbl,Dealer dlr){
 		while(!allSameBet){
 			blncMnyPlyrs=0;
-			for (Player plr : players) {
+			for (Player plr : players) {				
 				if(plr.getBet()==tbl.getHighsBet()||plr.isFold()){
 					if(plr.isFold()){
 						System.out.println('\n'+plr.getName()+" has fold");
 						System.out.println("================================");
+					}else{
+						System.out.println('\n'+plr.getName()+" has put the highst bet");
+						System.out.println("players hightst bet"+plr.getBet());
+						System.out.println("tables highest bet"+tbl.getHighsBet());
+						System.out.println("players hightst bet"+plr.getCash());
 					}
-					System.out.println('\n'+plr.getName()+" has put the highst bet");
-					System.out.println("players hightst bet"+plr.getBet());
-					System.out.println("tables highest bet"+tbl.getHighsBet());
-					System.out.println("players hightst bet"+plr.getCash());
 					blncMnyPlyrs++;
 				}else{
 					System.out.println(plr.getName()+" has not @@put the highst bet");
@@ -87,6 +99,7 @@ public class SnglPlayGame {
 				System.out.println("++++++++++++++++++++++++++++ all players have put highest bet");
 			}			
 		}
+		blncMnyPlyrs=0;		
 	}
 	
 	
