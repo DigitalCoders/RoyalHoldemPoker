5aa46231eb336533f4fd816a309b63b22117df64
Inesh-13208383 <Imesha@192.168.1.140>
2014-07-22 23:56:12 +0530

    AI check his own cards to decide what to do in his round

--- DigitalCoders/src/AIplayer.java
+++ DigitalCoders/src/AIplayer.java
@@ -21,4 +21,24 @@ public class AIplayer extends Player{
 	dlr.setBgBlind(input);
 	dlr.addTobetCollctn(input);
 	}
+	private  int chkMycrds(){
+		int myrnk=0;
+		if(crd1!=null&&crd2!=null){
+			myrnk+=crd1.getRankID();
+			myrnk+=crd2.getRankID();
+			if(crd1.getRankID()==crd2.getRankID()){
+				myrnk+=40;
+			}else if(crd1.getRankID()-1==crd2.getRankID()||crd1.getRankID()+1==crd2.getRankID()){
+				myrnk+=20;
+			}
+			if(crd1.getSuitID()==crd2.getSuitID()){
+				myrnk+=20;
+			}if(crd1.getRankID()==14||crd2.getRankID()==14){
+				myrnk+=30;
+			}
+			
+		}
+//		window.getLblBlind().setText("value "+myrnk);
+		return myrnk;
+	}
 }
